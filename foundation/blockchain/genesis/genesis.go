63aba12d3df47ad4796b41da719a5d52ff0ce854
zimblu <zimblu@kali.kali.com>
2024-02-29 10:24:41 +0800

    adding genesis

---
+++ foundation/blockchain/genesis/genesis.go
@@ -0,0 +1,35 @@
+package genesis
+
+import (
+	"encoding/json"
+	"os"
+	"time"
+)
+
+type Genesis struct {
+	Date          time.Time         `json:"date"`
+	ChainID       uint16            `json:"chain_id"`        // The chain id represents an unique id for this running instance.
+	TransPerBlock uint16            `json:"trans_per_block"` // The maximum number of transactions that can be in a block.
+	Difficulty    uint16            `json:"difficulty"`      // How difficult it needs to be to solve the work problem.
+	MiningReward  uint16            `json:"mining_reward"`   // Reward for mining a block.
+	GasPrice      uint16            `json:"gas_price"`       // Fee paid for each transaction mined into a block.
+	Balances      map[string]uint64 `json:"balances"`
+}
+
+// ------------------------------------------------------------------------------
+
+// Load opens and consumes the genesis file.
+
+func Load() (Genesis, error) {
+	path := "zblock/genesis.json"
+	content, err := os.ReadFile(path)
+	if err != nil {
+		return Genesis{}, err
+	}
+	var genesis Genesis
+	err = json.Unmarshal(content, &genesis)
+	if err != nil {
+		return Genesis{}, err
+	}
+	return genesis, nil
+}
