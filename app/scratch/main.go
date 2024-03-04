9ca8c4e8f37bc7f0ac630feee876189f2fd1a92a
zimblu <zimblu@kali.kali.com>
2024-03-04 14:04:25 +0800

    scratch digital signature 3

--- app/scratch/main.go
+++ app/scratch/main.go
@@ -28,7 +28,7 @@ func run() error {
 	}
 
 	tx := Tx{
-		FromID:  "bill",
+		FromID:  "0xF01813E4B85e178A83e29B8E7bF26BD830a25f32",
 		ToValue: "zimblu",
 		Value:   10000,
 	}
@@ -55,5 +55,43 @@ func run() error {
 
 	// ================================================================
 
+	tx = Tx{
+		FromID:  "0xF01813E4B85e178A83e29B8E7bF26BD830a25f32",
+		ToValue: "frank",
+		Value:   250,
+	}
+	data, err = json.Marshal(tx)
+	if err != nil {
+		fmt.Errorf("unable to Marshal: %w", err)
+	}
+	v2 := crypto.Keccak256(data)
+
+	sig2, err := crypto.Sign(v2, privateKey)
+	if err != nil {
+		fmt.Errorf("unable to sign: %w", err)
+	}
+	fmt.Println("SIG:", hexutil.Encode(sig2))
+
+	// ====================================================
+	// OVER THE WIRE
+
+	tx2 := Tx{
+		FromID:  "0xF01813E4B85e178A83e29B8E7bF26BD830a25f32",
+		ToValue: "frank",
+		Value:   250,
+	}
+
+	data, err = json.Marshal(tx2)
+	if err != nil {
+		fmt.Errorf("unable to Marshal: %w", err)
+	}
+	v2 = crypto.Keccak256(data)
+
+	publicKey, err = crypto.SigToPub(v2, sig2)
+	if err != nil {
+		return fmt.Errorf("unable to pub: %w", err)
+	}
+
+	fmt.Println("PUB:", crypto.PubkeyToAddress(*publicKey).String())
 	return nil
 }
