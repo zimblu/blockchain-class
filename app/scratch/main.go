e73936d28ffb572223b858b75cadc56e560e1cef
zimblu <zimblu@kali.kali.com>
2024-03-05 09:26:39 +0800

    transaction type 2

--- app/scratch/main.go
+++ app/scratch/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/big"
 
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
@@ -98,5 +100,28 @@ func run() error {
 	}
 
 	fmt.Println("PUB:", crypto.PubkeyToAddress(*publicKey).String())
+
+	vv, r, s, err := ToVRSFromHexSignature(hexutil.Encode(sig2))
+	if err != nil {
+		return fmt.Errorf("unable to VRS: %w", err)
+	}
+
+	fmt.Println("V|R|S", vv, r, s)
 	return nil
+}
+
+// ToVRSFromHexSignature converts a hex representation of the signature into
+// its R, S and V parts.
+
+func ToVRSFromHexSignature(sigStr string) (v, r, s *big.Int, err error) {
+	sig, err := hex.DecodeString(sigStr[2:])
+	if err != nil {
+		return nil, nil, nil, err
+	}
+
+	r = big.NewInt(0).SetBytes(sig[:32])
+	s = big.NewInt(0).SetBytes(sig[32:64])
+	v = big.NewInt(0).SetBytes([]byte{sig[64]})
+
+	return v, r, s, nil
 }
