9c65db7107d79826e13a0f48803f37cd45f252b3
zimblu <zimblu@kali.kali.com>
2024-03-12 15:01:08 +0800

    transaction type 4

--- app/scratch/main.go
+++ app/scratch/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"math/big"
 
+	"github.com/ardanlabs/blockchain/foundation/blockchain/database"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 )
@@ -107,6 +108,28 @@ func run() error {
 	}
 
 	fmt.Println("V|R|S", vv, r, s)
+
+	// ======================================================================
+
+	fmt.Println("=================================TX=====================================")
+
+	billTx, err := database.NewTx(1, 1,
+		"0xF01813E4B85e178A83e29B8E7bF26BD830a25f32",
+		"0xbEE6ACE826eC3DE1B6349888B9151B92522F7F76",
+		1000,
+		0,
+		nil)
+	if err != nil {
+		return fmt.Errorf("unable to billTx: %w", err)
+	}
+
+	signedTx, err := billTx.Sign(privateKey)
+	if err != nil {
+		return fmt.Errorf("unable to sign: %w", err)
+	}
+
+	fmt.Println(signedTx)
+
 	return nil
 }
 
